5c628a0958e02652e870733b29e24ca91e6f6495
ShiitakeMushroomCode <202144092@itc.ac.kr>
2024-10-26 21:29:02 +0900

    카무이

--- Project/src/app/layout.jsx
+++ Project/src/app/layout.jsx
@@ -10,6 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>{children}</body>
     </html>
   );
